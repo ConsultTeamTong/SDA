372835c892e8a1f1c30ff94d21c91a2c7ca07ddb
nonthana-Film <nonthanan@sala-daeng.com>
2026-04-10 18:17:24 +0700

    invoice, memo

--- Form-Layout/3. Purchasing - AP/7. AP Invoice/1.AP Invoice_ใบแจ้งหนี้/Main_1_Command.sql
+++ Form-Layout/3. Purchasing - AP/7. AP Invoice/1.AP Invoice_ใบแจ้งหนี้/Main_1_Command.sql
@@ -1,208 +1,211 @@
 ﻿SELECT DISTINCT
 CONCAT(OCPR.FirstName,' ',OCPR.LastName) AS 'Coontact',
 BRANCH.Code ,
-CASE WHEN BRANCH.Code = '00000' AND OPCH.DocCur = OADM.MainCurncy THEN N'สำนักงานใหญ่' 
-  WHEN BRANCH.Code = '00000' AND OPCH.DocCur <> OADM.MainCurncy THEN 'Head office' 
-  WHEN BRANCH.Code <> '00000' AND OPCH.DocCur = OADM.MainCurncy THEN concat(N'สาขาที่' ,' ',BRANCH.Code) 
-  WHEN BRANCH.Code <> '00000' AND OPCH.DocCur <> OADM.MainCurncy THEN concat('Branch' ,' ',BRANCH.Code) 
+CASE WHEN BRANCH.Code = '00000' AND ORPC.DocCur = OADM.MainCurncy THEN N'สำนักงานใหญ่' 
+  WHEN BRANCH.Code = '00000' AND ORPC.DocCur <> OADM.MainCurncy THEN 'Head office' 
+  WHEN BRANCH.Code <> '00000' AND ORPC.DocCur = OADM.MainCurncy THEN concat(N'สาขาที่' ,' ',BRANCH.Code) 
+  WHEN BRANCH.Code <> '00000' AND ORPC.DocCur <> OADM.MainCurncy THEN concat('Branch' ,' ',BRANCH.Code) 
 END 'GLN_H' ,
-CASE WHEN CRD.GlblLocNum = '00000' AND OPCH.DocCur = OADM.MainCurncy THEN N'(สำนักงานใหญ่)' 
-  WHEN CRD.GlblLocNum = '00000' AND OPCH.DocCur <> OADM.MainCurncy THEN '(Head office)' 
-  WHEN CRD.GlblLocNum <> '00000' AND OPCH.DocCur = OADM.MainCurncy THEN concat(N'(สาขาที่' ,' ',CRD.GlblLocNum,')') 
-  WHEN CRD.GlblLocNum <> '00000' AND OPCH.DocCur <> OADM.MainCurncy THEN concat('(Branch' ,' ',CRD.GlblLocNum,')') 
-  when CRD.GlblLocNum = '' or CRD1.GlblLocNum is null then ''
+CASE WHEN CRD1.GlblLocNum = '00000' AND ORPC.DocCur = OADM.MainCurncy THEN N'(สำนักงานใหญ่)' 
+  WHEN CRD1.GlblLocNum = '00000' AND ORPC.DocCur <> OADM.MainCurncy THEN '(Head office)' 
+  WHEN CRD1.GlblLocNum <> '00000' AND ORPC.DocCur = OADM.MainCurncy THEN concat(N'(สาขาที่' ,' ',CRD1.GlblLocNum,')') 
+  WHEN CRD1.GlblLocNum <> '00000' AND ORPC.DocCur <> OADM.MainCurncy THEN concat('(Branch' ,' ',CRD1.GlblLocNum,')') 
+  when CRD1.GlblLocNum = '' or CRD1.GlblLocNum is null then ''
 END 'GLN_BP' ,
  CASE 
- WHEN OPCH.Printed = 'N' AND OPCH.DocCur <> OADM.MainCurncy THEN 'Original'
- WHEN OPCH.Printed = 'N' AND OPCH.DocCur = OADM.MainCurncy THEN N'ต้นฉบับ' 
- WHEN OPCH.Printed = 'Y' AND OPCH.DocCur <> OADM.MainCurncy THEN 'Copy'  
- WHEN OPCH.Printed = 'Y' AND OPCH.DocCur = OADM.MainCurncy THEN N'สำเนา'
+ WHEN ORPC.Printed = 'N' AND ORPC.DocCur <> OADM.MainCurncy THEN 'Original'
+ WHEN ORPC.Printed = 'N' AND ORPC.DocCur = OADM.MainCurncy THEN N'ต้นฉบับ' 
+ WHEN ORPC.Printed = 'Y' AND ORPC.DocCur <> OADM.MainCurncy THEN 'Copy'  
+ WHEN ORPC.Printed = 'Y' AND ORPC.DocCur = OADM.MainCurncy THEN N'สำเนา'
  END AS 'Print Status',
 BRANCH.[Name] As 'BranchName',
 BRANCH.U_SLD_VTAXID As 'TaxIdNum',
 BRANCH.U_SLD_VComName As 'PrintHeadr',
 BRANCH.U_SLD_F_VComName As 'PrintHdrF',
-CASE WHEN OPCH.DocCur = OADM.MainCurncy THEN BRANCH.U_SLD_Building ELSE BRANCH.U_SLD_F_Building END AS 'Building',
-CASE WHEN OPCH.DocCur = OADM.MainCurncy THEN BRANCH.U_SLD_Steet  ELSE BRANCH.U_SLD_F_Steet  END AS 'Street',
-CASE WHEN OPCH.DocCur = OADM.MainCurncy THEN BRANCH.U_SLD_Block  ELSE BRANCH.U_SLD_F_Block   END AS 'Block',
-CASE WHEN OPCH.DocCur = OADM.MainCurncy THEN BRANCH.U_SLD_City  ELSE BRANCH.U_SLD_F_City  END As 'City',
-CASE WHEN OPCH.DocCur = OADM.MainCurncy THEN BRANCH.U_SLD_County ELSE BRANCH.U_SLD_F_County  END As 'County',
+CASE WHEN ORPC.DocCur = OADM.MainCurncy THEN BRANCH.U_SLD_Building ELSE BRANCH.U_SLD_F_Building END AS 'Building',
+CASE WHEN ORPC.DocCur = OADM.MainCurncy THEN BRANCH.U_SLD_Steet  ELSE BRANCH.U_SLD_F_Steet  END AS 'Street',
+CASE WHEN ORPC.DocCur = OADM.MainCurncy THEN BRANCH.U_SLD_Block  ELSE BRANCH.U_SLD_F_Block   END AS 'Block',
+CASE WHEN ORPC.DocCur = OADM.MainCurncy THEN BRANCH.U_SLD_City  ELSE BRANCH.U_SLD_F_City  END As 'City',
+CASE WHEN ORPC.DocCur = OADM.MainCurncy THEN BRANCH.U_SLD_County ELSE BRANCH.U_SLD_F_County  END As 'County',
 BRANCH.U_SLD_ZipCode As 'ZipCode',
 BRANCH.U_SLD_Tel As 'Tel',
 BRANCH.U_SLD_Fax As 'BFax',
 BRANCH.U_SLD_Email AS 'E-Mail',
 --------------------------------------------------------------------------------------------------------
-OPCH.DocEntry,
-OPCH.[Address],
-OPCH.CardCode,
+ORPC.[Address],
+ORPC.VatSumFC, 
+ORPC.DocTotalFC,
+--ORPC.U_CN_01,
+--ORPC.U_CN_02,
+--ORPC.U_CN_03,
 OCRD.U_SLD_Title,
 OCRD.U_SLD_FullName,
+--CASE WHEN CRD1.GlblLocNum IS NULL THEN ''
+  --WHEN CRD1.GlblLocNum IS NOT NULL THEN N'สาขาที่ ' + CRD1.GlblLocNum
+  --END 'GLN',
 CASE WHEN OCRD.Phone2 IS NULL THEN ''
   WHEN OCRD.Phone2 IS NOT NULL THEN ', ' + OCRD.Phone2
   END 'Phone2',
 OCRD.Phone1, 
-OCRD.Fax,
-OCRD.LicTradNum,  
+OCRD.Fax, 
+OSLP.SlpName,
 NNM1.BeginStr, 
-OPCH.DocNum, 
-OPCH.DocDate, 
-OPCH.DocDueDate, 
-OCTG.PymntGroup, 
-ISNULL(OPCH.NumAtCard,'') AS 'NumAtCard',
-(PCH1.VisOrder) AS 'No.', 
-PCH1.LineNum as 'Line No.', 
-PCH1.ItemCode, 
-PCH1.Dscription as 'Dscription', 
-PCH1.Quantity,
-PCH1.PriceBefDi, 
-PCH1.DiscPrcnt As 'LDiscPrcnt', 
-PCH1.LineTotal,
-PCH1.TotalFrgn, 
-OPCH.VatSum,
-OPCH.VatSumFC,
-OPCH.DiscSum,
-OPCH.DiscSumFC, 
-OPCH.DiscPrcnt,
-OPCH.DocCur, 
-OPCH.DocTotal,
-OPCH.DocTotalFC,
-PCH1.unitMsr,
-OPCH.Comments,
-OPCH.DpmAmnt,
-OPCH.DpmAmntFC,
-PCH1.LineType,
-PCH1.Project,
-OCPR.Name,
-OCPR.Tel1,
-OCPR.Tel2,
-OCPR.E_mailL,
-PCH12.StreetB,
-PCH12.StreetNoB,
-PCH12.BlockB,
-PCH12.CityB,
-PCH12.ZipCodeB,
-PCH12.CountyB,
-PCH12.CountryB
+ORPC.DocEntry, 
+ORPC.DocNum, 
+ORPC.DocDate,
+--CASE WHEN ORPC.U_CN_05 IS not NULL THEN ''
+  --WHEN ORPC.U_CN_05 IS NULL THEN T10.[Name]
+  --END 'Reason_DB',
+--CASE WHEN ORPC.U_CN_05 IS NULL THEN ''
+  --WHEN ORPC.U_CN_05 IS NOT NULL THEN N'ลดหนี้เนื่องจาก' + ORPC.U_CN_05
+  --END 'Reason_CN_Remark',
 
-FROM OPCH   
-INNER JOIN PCH1 ON OPCH.DocEntry = PCH1.DocEntry 
-INNER JOIN PCH12 ON OPCH.DocEntry = PCH12.DocEntry 
-LEFT JOIN OITM ON PCH1.ItemCode = OITM.ItemCode 
-LEFT JOIN OCRD ON OPCH.CardCode = OCRD.CardCode 
-LEFT JOIN CRD1 ON OCRD.CardCode = CRD1.CardCode 
-LEFT JOIN OCPR ON OPCH.CntctCode = OCPR.CntctCode 
-LEFT JOIN NNM1 ON OPCH.Series = NNM1.Series 
-LEFT JOIN OCTG ON OPCH.GroupNum = OCTG.GroupNum
-LEFT JOIN OHEM ON OPCH.OwnerCode = OHEM.empID
-LEFT JOIN CRD1 CRD ON (OPCH.PaytoCode = CRD.[Address] AND OPCH.CardCode = CRD.CardCode  AND CRD.AdresType ='B' ) 
-LEFT JOIN OPRJ ON PCH1.Project = OPRJ.PrjCode
-LEFT JOIN OUSR ON OPCH.UserSign = OUSR.USERID
-LEFT JOIN [dbo].[@SLDT_SET_BRANCH] BRANCH ON OPCH.U_SLD_LVatBranch = BRANCH.Code , oadm
+(RPC1.VisOrder) AS 'No.', 
+RPC1.LineNum as 'Line No.', 
 
-WHERE OPCH.DocEntry = {?DocKey@}
+RPC1.ItemCode, 
 
-Union all
+RPC1.Dscription as 'Dscription', 
+RPC1.LineType as 'LineType',
+
+RPC1.Quantity, 
+RPC1.UomCode,
+RPC1.unitMsr,  
+RPC1.PriceBefDi, 
+RPC1.LineTotal,
+RPC1.TotalFrgn, 
+ORPC.VatSum,
+ORPC.DocCur, 
+ORPC.DocTotal,
+OCRD.LicTradNum,
+ORPC.Printed,
+ORPC.Comments,
+RPC1.Project,
+RPC12.StreetB,
+RPC12.StreetNoB,
+RPC12.BlockB,
+RPC12.CityB,
+RPC12.ZipcodeB,
+RPC12.CountyB,
+RPC12.CountryB
+
+FROM ORPC 
+INNER JOIN RPC1 ON ORPC.DocEntry = RPC1.DocEntry 
+INNER JOIN RPC12 ON ORPC.DocEntry = RPC12.DocEntry 
+LEFT JOIN OITM ON RPC1.ItemCode = OITM.ItemCode 
+LEFT JOIN OCRD ON ORPC.CardCode = OCRD.CardCode 
+LEFT JOIN CRD1 ON OCRD.CardCode = CRD1.CardCode AND CRD1.AdresType = 'B' AND CRD1.[Address] = ORPC.PayToCode
+LEFT JOIN OCPR ON ORPC.CntctCode = OCPR.CntctCode 
+LEFT JOIN NNM1 ON ORPC.Series = NNM1.Series 
+LEFT JOIN OCTG ON ORPC.GroupNum = OCTG.GroupNum
+LEFT JOIN OHEM ON ORPC.OwnerCode = OHEM.empID
+LEFT JOIN OSLP ON ORPC.SlpCode = OSLP.SlpCode
+--LEFT JOIN [dbo].[@SLD_REASON_RD] T10 ON ORPC.U_CN_04 = T10.code
+LEFT JOIN CRD1 CRD ON (ORPC.PaytoCode = CRD.[Address] AND ORPC.CardCode = CRD.CardCode AND CRD.AdresType ='B' ) 
+LEFT JOIN OUSR ON ORPC.UserSign = OUSR.USERID
+LEFT JOIN OPRJ ON RPC1.Project = OPRJ.PrjCode
+LEFT JOIN [dbo].[@SLDT_SET_BRANCH] BRANCH ON ORPC.U_SLD_LVatBranch = BRANCH.Code , oadm
+
+WHERE ORPC.DocEntry  = 1
+
+Union all 
 SELECT DISTINCT
-CONCAT(OCPR.FirstName,' ',OCPR.LastName) AS 'Coontact',
-BRANCH.Code ,
-CASE WHEN BRANCH.Code = '00000' AND OPCH.DocCur = OADM.MainCurncy THEN N'สำนักงานใหญ่' 
-  WHEN BRANCH.Code = '00000' AND OPCH.DocCur <> OADM.MainCurncy THEN 'Head office' 
-  WHEN BRANCH.Code <> '00000' AND OPCH.DocCur = OADM.MainCurncy THEN concat(N'สาขาที่' ,' ',BRANCH.Code) 
-  WHEN BRANCH.Code <> '00000' AND OPCH.DocCur <> OADM.MainCurncy THEN concat('Branch' ,' ',BRANCH.Code) 
+CONCAT(OCPR.FirstName,' ',OCPR.LastName) AS 'Coontact', BRANCH.Code ,
+CASE WHEN BRANCH.Code = '00000' AND ORPC.DocCur = OADM.MainCurncy THEN N'สำนักงานใหญ่' 
+  WHEN BRANCH.Code = '00000' AND ORPC.DocCur <> OADM.MainCurncy THEN 'Head office' 
+  WHEN BRANCH.Code <> '00000' AND ORPC.DocCur = OADM.MainCurncy THEN concat(N'สาขาที่' ,' ',BRANCH.Code) 
+  WHEN BRANCH.Code <> '00000' AND ORPC.DocCur <> OADM.MainCurncy THEN concat('Branch' ,' ',BRANCH.Code) 
 END 'GLN_H' ,
-CASE WHEN CRD.GlblLocNum = '00000' AND OPCH.DocCur = OADM.MainCurncy THEN N'(สำนักงานใหญ่)' 
-  WHEN CRD.GlblLocNum = '00000' AND OPCH.DocCur <> OADM.MainCurncy THEN '(Head office)' 
-  WHEN CRD.GlblLocNum <> '00000' AND OPCH.DocCur = OADM.MainCurncy THEN concat(N'(สาขาที่' ,' ',CRD.GlblLocNum,')') 
-  WHEN CRD.GlblLocNum <> '00000' AND OPCH.DocCur <> OADM.MainCurncy THEN concat('(Branch' ,' ',CRD.GlblLocNum,')') 
-  when CRD.GlblLocNum = '' or CRD1.GlblLocNum is null then ''
+CASE WHEN CRD1.GlblLocNum = '00000' AND ORPC.DocCur = OADM.MainCurncy THEN N'(สำนักงานใหญ่)' 
+  WHEN CRD1.GlblLocNum = '00000' AND ORPC.DocCur <> OADM.MainCurncy THEN '(Head office)' 
+  WHEN CRD1.GlblLocNum <> '00000' AND ORPC.DocCur = OADM.MainCurncy THEN concat(N'(สาขาที่' ,' ',CRD1.GlblLocNum,')') 
+  WHEN CRD1.GlblLocNum <> '00000' AND ORPC.DocCur <> OADM.MainCurncy THEN concat('(Branch' ,' ',CRD1.GlblLocNum,')') 
+  when CRD1.GlblLocNum = '' or CRD1.GlblLocNum is null then ''
 END 'GLN_BP' ,
  CASE 
- WHEN OPCH.Printed = 'N' AND OPCH.DocCur <> OADM.MainCurncy THEN 'Original'
- WHEN OPCH.Printed = 'N' AND OPCH.DocCur = OADM.MainCurncy THEN N'ต้นฉบับ' 
- WHEN OPCH.Printed = 'Y' AND OPCH.DocCur <> OADM.MainCurncy THEN 'Copy'  
- WHEN OPCH.Printed = 'Y' AND OPCH.DocCur = OADM.MainCurncy THEN N'สำเนา'
+ WHEN ORPC.Printed = 'N' AND ORPC.DocCur <> OADM.MainCurncy THEN 'Original'
+ WHEN ORPC.Printed = 'N' AND ORPC.DocCur = OADM.MainCurncy THEN N'ต้นฉบับ' 
+ WHEN ORPC.Printed = 'Y' AND ORPC.DocCur <> OADM.MainCurncy THEN 'Copy'  
+ WHEN ORPC.Printed = 'Y' AND ORPC.DocCur = OADM.MainCurncy THEN N'สำเนา'
  END AS 'Print Status',
 BRANCH.[Name] As 'BranchName',
 BRANCH.U_SLD_VTAXID As 'TaxIdNum',
 BRANCH.U_SLD_VComName As 'PrintHeadr',
 BRANCH.U_SLD_F_VComName As 'PrintHdrF',
-CASE WHEN OPCH.DocCur = OADM.MainCurncy THEN BRANCH.U_SLD_Building ELSE BRANCH.U_SLD_F_Building END AS 'Building',
-CASE WHEN OPCH.DocCur = OADM.MainCurncy THEN BRANCH.U_SLD_Steet  ELSE BRANCH.U_SLD_F_Steet  END AS 'Street',
-CASE WHEN OPCH.DocCur = OADM.MainCurncy THEN BRANCH.U_SLD_Block  ELSE BRANCH.U_SLD_F_Block   END AS 'Block',
-CASE WHEN OPCH.DocCur = OADM.MainCurncy THEN BRANCH.U_SLD_City  ELSE BRANCH.U_SLD_F_City  END As 'City',
-CASE WHEN OPCH.DocCur = OADM.MainCurncy THEN BRANCH.U_SLD_County ELSE BRANCH.U_SLD_F_County  END As 'County',
+CASE WHEN ORPC.DocCur = OADM.MainCurncy THEN BRANCH.U_SLD_Building ELSE BRANCH.U_SLD_F_Building END AS 'Building',
+CASE WHEN ORPC.DocCur = OADM.MainCurncy THEN BRANCH.U_SLD_Steet  ELSE BRANCH.U_SLD_F_Steet  END AS 'Street',
+CASE WHEN ORPC.DocCur = OADM.MainCurncy THEN BRANCH.U_SLD_Block  ELSE BRANCH.U_SLD_F_Block   END AS 'Block',
+CASE WHEN ORPC.DocCur = OADM.MainCurncy THEN BRANCH.U_SLD_City  ELSE BRANCH.U_SLD_F_City  END As 'City',
+CASE WHEN ORPC.DocCur = OADM.MainCurncy THEN BRANCH.U_SLD_County ELSE BRANCH.U_SLD_F_County  END As 'County',
 BRANCH.U_SLD_ZipCode As 'ZipCode',
 BRANCH.U_SLD_Tel As 'Tel',
 BRANCH.U_SLD_Fax As 'BFax',
 BRANCH.U_SLD_Email AS 'E-Mail',
 --------------------------------------------------------------------------------------------------------
-OPCH.DocEntry,
-OPCH.[Address],
-OPCH.CardCode,
+ORPC.[Address],
+ORPC.VatSumFC, 
+ORPC.DocTotalFC,
+--ORPC.U_CN_01,
+--ORPC.U_CN_02,
+--ORPC.U_CN_03,
 OCRD.U_SLD_Title,
 OCRD.U_SLD_FullName,
+--CASE WHEN CRD1.GlblLocNum IS NULL THEN ''
+  --WHEN CRD1.GlblLocNum IS NOT NULL THEN N'สาขาที่ ' + CRD1.GlblLocNum
+  --END 'GLN',
 CASE WHEN OCRD.Phone2 IS NULL THEN ''
   WHEN OCRD.Phone2 IS NOT NULL THEN ', ' + OCRD.Phone2
   END 'Phone2',
 OCRD.Phone1, 
-OCRD.Fax,
-OCRD.LicTradNum,  
+OCRD.Fax, 
+OSLP.SlpName,
 NNM1.BeginStr, 
-OPCH.DocNum, 
-OPCH.DocDate, 
-OPCH.DocDueDate, 
-OCTG.PymntGroup, 
-ISNULL(OPCH.NumAtCard,'') AS 'NumAtCard',
-(PCH10.AftLineNum + 0.5) AS 'No.', 
-PCH10.LineSeq as 'Line No.', 
+ORPC.DocEntry, 
+ORPC.DocNum, 
+ORPC.DocDate,
+--CASE WHEN ORPC.U_CN_05 IS not NULL THEN ''
+  --WHEN ORPC.U_CN_05 IS NULL THEN T10.[Name]
+  --END 'Reason_DB',
+--CASE WHEN ORPC.U_CN_05 IS NULL THEN ''
+  --WHEN ORPC.U_CN_05 IS NOT NULL THEN N'ลดหนี้เนื่องจาก' + ORPC.U_CN_05
+  --END 'Reason_CN_Remark',
+(RPC10.AftLineNum + 0.5) AS 'No.', 
+RPC10.LineSeq as 'Line No.', 
 '' as ItemCode, 
-CAST(PCH10.LineText as nvarchar(4000)) as 'Dscription', 
-'0' as Quantity,
+CAST(RPC10.LineText as nvarchar(4000)) as 'Dscription', 
+RPC10.LineType as 'LineType',
+'0' as Quantity, 
+'' as UomCode,
+'' as unitMsr,  
 '0' as PriceBefDi, 
-'0' as LDiscPrcnt, 
 '0' as LineTotal,
 '0' as TotalFrgn, 
-OPCH.VatSum,
-OPCH.VatSumFC,
-OPCH.DiscSum,
-OPCH.DiscSumFC, 
-OPCH.DiscPrcnt,
-OPCH.DocCur, 
-OPCH.DocTotal,
-OPCH.DocTotalFC,
-'' as unitMsr,
-OPCH.Comments,
-OPCH.DpmAmnt,
-OPCH.DpmAmntFC,
-PCH10.LineType,
-PCH1.Project,
-OCPR.Name,
-OCPR.Tel1,
-OCPR.Tel2,
-OCPR.E_mailL,
-PCH12.StreetB,
-PCH12.StreetNoB,
-PCH12.BlockB,
-PCH12.CityB,
-PCH12.ZipCodeB,
-PCH12.CountyB,
-PCH12.CountryB
+ORPC.VatSum,
+ORPC.DocCur, 
+ORPC.DocTotal,
+OCRD.LicTradNum,
+ORPC.Printed,
+ORPC.Comments,
+RPC1.Project
 
-FROM OPCH   
-INNER JOIN PCH1 ON OPCH.DocEntry = PCH1.DocEntry
-INNER JOIN PCH10 ON OPCH.DocEntry = PCH10.DocEntry
-INNER JOIN PCH12 ON OPCH.DocEntry = PCH12.DocEntry 
---LEFT JOIN OITM ON PCH1.ItemCode = OITM.ItemCode 
-LEFT JOIN OCRD ON OPCH.CardCode = OCRD.CardCode 
-LEFT JOIN CRD1 ON OCRD.CardCode = CRD1.CardCode 
-LEFT JOIN OCPR ON OPCH.CntctCode = OCPR.CntctCode 
-LEFT JOIN NNM1 ON OPCH.Series = NNM1.Series 
-LEFT JOIN OCTG ON OPCH.GroupNum = OCTG.GroupNum
-LEFT JOIN OHEM ON OPCH.OwnerCode = OHEM.empID
-LEFT JOIN CRD1 CRD ON (OPCH.PaytoCode = CRD.[Address] AND OPCH.CardCode = CRD.CardCode  AND CRD.AdresType ='B' ) 
---LEFT JOIN OPRJ ON PCH1.Project = OPRJ.PrjCode
-LEFT JOIN OUSR ON OPCH.UserSign = OUSR.USERID
-LEFT JOIN [dbo].[@SLDT_SET_BRANCH] BRANCH ON OPCH.U_SLD_LVatBranch = BRANCH.Code , oadm
+FROM ORPC 
+INNER JOIN RPC10 ON ORPC.DocEntry = RPC10.DocEntry 
+INNER JOIN RPC1 ON ORPC.DocEntry = RPC1.DocEntry 
+INNER JOIN RPC12 ON ORPC.DocEntry = RPC12.DocEntry 
+LEFT JOIN OITM ON RPC1.ItemCode = OITM.ItemCode 
+LEFT JOIN OCRD ON ORPC.CardCode = OCRD.CardCode 
+LEFT JOIN CRD1 ON OCRD.CardCode = CRD1.CardCode AND CRD1.AdresType = 'B' AND CRD1.[Address] = ORPC.PayToCode
+LEFT JOIN OCPR ON ORPC.CntctCode = OCPR.CntctCode 
+LEFT JOIN NNM1 ON ORPC.Series = NNM1.Series 
+LEFT JOIN OCTG ON ORPC.GroupNum = OCTG.GroupNum
+LEFT JOIN OHEM ON ORPC.OwnerCode = OHEM.empID
+LEFT JOIN OSLP ON ORPC.SlpCode = OSLP.SlpCode
+--LEFT JOIN [dbo].[@SLD_REASON_RD] T10 ON ORPC.U_CN_04 = T10.code
+LEFT JOIN CRD1 CRD ON (ORPC.PaytoCode = CRD.[Address] AND ORPC.CardCode = CRD.CardCode AND CRD.AdresType ='B' ) 
+LEFT JOIN OUSR ON ORPC.UserSign = OUSR.USERID
+LEFT JOIN OPRJ ON RPC1.Project = OPRJ.PrjCode
+LEFT JOIN [dbo].[@SLDT_SET_BRANCH] BRANCH ON ORPC.U_SLD_LVatBranch = BRANCH.Code , oadm
 
-WHERE OPCH.DocEntry = {?DocKey@}
+WHERE ORPC.DocEntry  = 1
+
 Order by 'No.' , 'Line No.'
