a5cab7d44c49715cd78f8d4026177fee19d1f45b
nonthana-Film <nonthanan@sala-daeng.com>
2026-05-07 12:04:31 +0700

    ENG From Add

--- Form-Layout/2. Sales - AR/4. Return/2.Retrun_ใบรับคืนสินค้า_(Batch_Serial)/Main_ENG.sql
+++ Form-Layout/2. Sales - AR/4. Return/2.Retrun_ใบรับคืนสินค้า_(Batch_Serial)/Main_ENG.sql
@@ -1,4 +1,4 @@
-﻿SELECT distinct
+SELECT distinct
 RDN12.StreetB     AS '1Bill',
     RDN12.StreetNoB   AS '2Bill',
     RDN12.BlockB      AS '3Bill',
@@ -14,7 +14,8 @@ RDN12.StreetB     AS '1Bill',
 ORDN.[CardCode],
 ORDN.[Comments],
 RDN1.[ItemCode],
-RDN1.[dscription] as 'Dscription',
+--RDN1.[dscription] as 'Dscription',
+OITM.FrgnName as 'Dscription',
 RDN1.[Quantity],
 ORDN.[DocDate],
 ORDN.[DocNum],
@@ -39,13 +40,13 @@ CASE WHEN OCRD.Phone2 IS NULL THEN ''
   WHEN OCRD.Phone2 IS NOT NULL THEN ', ' + OCRD.Phone2
   END 'Phone2',
 RDN1.LineType,
-OCPR.Name,
+--OCPR.Name,
+OSLP.U_Name_Foreign as 'Name',
 OCPR.Tel1,
 OCPR.E_MailL,
 RDN1.LineNum,
-RDN10.AftLineNum
-
-
+RDN10.AftLineNum,
+OUGP.UgpCode
 FROM ORDN  
 INNER JOIN RDN1 ON ORDN.[DocEntry] = RDN1.[DocEntry]
 LEFT JOIN RDN10 ON RDN1.[DocEntry] = RDN10.[DocEntry] AND RDN1.LineNum = RDN10.AftLineNum 
@@ -56,5 +57,8 @@ LEFT JOIN CRD1 ON (ORDN.[CardCode] = CRD1.[CardCode] AND ORDN.[PayToCode] = CRD1
 LEFT JOIN NNM1 ON ORDN.[Series] = NNM1.[Series]
 LEFT JOIN OUSR ON ORDN.UserSign = OUSR.USERID
 LEFT JOIN OPRJ ON RDN1.Project = OPRJ.PrjCode
+LEFT Join OITM ON RDN1.ItemCode = OITM.ItemCode 
+LEFT JOIN OUGP ON RDN1.UomCode = OUGP.UgpCode
+LEFT JOIN OSLP ON ORDN.SLPCODE = OSLP.SLPCODE 
 LEFT JOIN [dbo].[@SLDT_SET_BRANCH] BRANCH ON ORDN.U_SLD_LVatBranch = BRANCH.Code , oadm
-WHERE ORDN.[DocEntry] = 1--{?DocKey@}
+WHERE ORDN.[DocEntry] = {?DocKey@}
